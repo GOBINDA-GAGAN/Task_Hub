18b2c849819f4ba055ebb80dd0438809565d6139
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-06-07 10:25:30 +0530

    Add error handler middleware

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,48 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import { notFoundPage, errorHandler } from "./middleware/error.js";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//? basic home route
 app.get("/", (req, res) => {
-  console.log("hello world");
+  res.status(200).json({
+    message: "Welcome to TaskHub Api",
+  });
 });
+
+
+
+
+
+
+//? error middleware
+app.use(errorHandler);
+
+//? not found page  middleware
+app.use(notFoundPage);
+
 app.listen(PORT, () => {
   console.log("server started at port:" + PORT + " 🏡");
 });
